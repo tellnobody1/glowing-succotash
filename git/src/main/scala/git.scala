80cb3f6ddce9a7433edc6fef2c1a354688a495f7
Andrii Nemchenko <a.nemchenko@icloud.com>
2020-02-04 19:40:20 +0200

    fix null

--- git/src/main/scala/git.scala
+++ git/src/main/scala/git.scala
@@ -7,7 +7,7 @@ object GitOps {
 
   def version(dir: String = ".", tags: Boolean = false, stripPrefix: String = "v"): String = {
     val git = org.eclipse.jgit.api.Git.open(new File(dir))
-    val desc = Some(git.describe.setTags(tags).call).map(_.stripPrefix(stripPrefix))
+    val desc = Option(git.describe.setTags(tags).call).map(_.stripPrefix(stripPrefix))
     val repo = git.getRepository
     val base = desc.getOrElse("0-"+repo.newObjectReader.abbreviate(repo.resolve("HEAD")).name)
     val dirty = if (git.status.call.isClean) "" else "-dirty"
