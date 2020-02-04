70d664fa6be698e7f7ad79eff45718172727cfcf
Andrii Nemchenko <a.nemchenko@icloud.com>
2020-02-04 19:36:46 +0200

    git version for tagless repo

--- git/src/main/scala/git.scala
+++ git/src/main/scala/git.scala
@@ -6,9 +6,11 @@ object GitOps {
   def version: String = version()
 
   def version(dir: String = ".", tags: Boolean = false, stripPrefix: String = "v"): String = {
-    val repo = org.eclipse.jgit.api.Git.open(new File(dir))
-    val desc = repo.describe.setTags(tags).call.stripPrefix(stripPrefix)
-    val dirty = if (repo.status.call.isClean) "" else "-dirty"
-    s"${desc}${dirty}"
+    val git = org.eclipse.jgit.api.Git.open(new File(dir))
+    val desc = Some(git.describe.setTags(tags).call).map(_.stripPrefix(stripPrefix))
+    val repo = git.getRepository
+    val base = desc.getOrElse("0-"+repo.newObjectReader.abbreviate(repo.resolve("HEAD")).name)
+    val dirty = if (git.status.call.isClean) "" else "-dirty"
+    s"${base}${dirty}"
   }
 }
