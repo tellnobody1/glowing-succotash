d0abdf9832f75b79be1ad63efeaedcd1141078f4
Andrey Nemchenko <a.nemchenko@icloud.com>
2020-05-25 14:45:34 +0300

    add map_ for option

--- src/main/scala/option.scala
+++ src/main/scala/option.scala
@@ -10,6 +10,7 @@ package object option {
   }
 
   implicit class OptionExt[A](x: Option[A]) {
+    def map_[U](f: A => U): Unit = x.foreach(f)
     def cata[B](f: A => B, b: => B): B = x match {
       case Some(a) => f(a)
       case None => b
