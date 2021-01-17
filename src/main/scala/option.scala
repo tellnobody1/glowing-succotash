31796b0150a1f3843ffd2724623884c091440ab0
Andrii Nemchenko <a.nemchenko@icloud.com>
2021-01-17 16:40:33 +0200

    null

--- src/main/scala/option.scala
+++ src/main/scala/option.scala
@@ -1,19 +1,19 @@
 package zero.ext
+package option
 
-package object option {
-  val none = None
+val none = None
 
-  inline def fromNullable[A](x: A): Option[A] = Option(x)
+extension [A](x: A | Null)
+  inline def toOption: Option[A] = if x != null then x.nn.some else none
 
-  extension [A](x: A)
-    inline def some: Some[A] = Some(x)
+extension [A](x: A)
+  inline def some: Some[A] = Some(x)
 
-  extension [A,B,U](x: Option[A])
-    inline def map_(f: A => U): Unit = x.foreach(f)
-    inline def cata(f: A => B, b: => B): B = x.fold(b)(f)
-    inline def void: Option[Unit] = x.map(_ => ())
-    inline def tap(f: A => U): Option[A] = x.map{r =>
-      f(r)
-      r
-    }
-}
+extension [A,B,U](x: Option[A])
+  inline def map_(f: A => U): Unit = x.foreach(f)
+  inline def cata(f: A => B, b: => B): B = x.fold(b)(f)
+  inline def void: Option[Unit] = x.map(_ => ())
+  inline def tap(f: A => U): Option[A] = x.map{r =>
+    f(r)
+    r
+  }
