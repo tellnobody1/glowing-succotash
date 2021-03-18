2fc031d35f106beec009df6afe24787cdf47d314
Andrii Nemchenko <a.nemchenko@icloud.com>
2021-03-18 23:33:40 +0200

    IArray support

--- src/main/scala-3.0.0-RC1/bytes.scala
+++ src/main/scala-3.0.0-RC1/bytes.scala
@@ -1,7 +1,7 @@
 package zero.ext
 
 extension (x: Array[Byte])
-  inline def hex: Array[Byte] =
+  inline def _hex: Array[Byte] =
     val acc = new Array[Byte](x.length * 2)
     var i = 0
     while (i < x.length) {
@@ -11,8 +11,24 @@ extension (x: Array[Byte])
       i += 1
     }
     acc
-  inline def utf8: String =
+
+  inline def _utf8: String =
     String(x, "utf8")
+
+extension (x: IArray[Byte])
+  inline def hex: IArray[Byte] =
+    val acc = new Array[Byte](x.length * 2)
+    var i = 0
+    while (i < x.length) {
+      val v = x(i) & 0xff
+      acc(i * 2) = hexs(v >>> 4)
+      acc(i * 2 + 1) = hexs(v & 0x0f)
+      i += 1
+    }
+    IArray.unsafeFromArray(acc)
+
+  inline def utf8: String =
+    String(x.toArray, "utf8")
 
 extension (x: String)
   inline def utf8: Array[Byte] =
